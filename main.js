0b277f4cf7da95f9f110135f5a5df581db755d4c
alyssa-hdz <alyssa.hernandez619@gmail.com>
2025-02-26 14:07:18 -0600

    Added sections to JS

--- main.js
+++ main.js
@@ -7,7 +7,54 @@ const data =await response.json();
     title: book.title_suggest[0],
     author: book.author_name[0],
     publicationYear: book.first_publish_year,
-    isbn: book.isbn_13[0]
+    cover: {
+        i: book.cover_i? `https://covers.openlibrary.org/b/isbn/${book.cover_i}-M.jpg` : 'No cover image'
+    }
  }));
 
 }
+
+//search books//
+async function searchBooks(query)
+{
+    const books=await fetchBooks(query);
+    return books;
+}
+
+//render books//
+
+function renderBooks(books) {
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+    if(books.length === 0) return;
+    books.forEach(book => {
+        const bookElement = document.createElement('li');
+        bookElement.innerHTML = `
+            <h2>${book.title}</h2>
+            <p>Author: ${book.author}</p>
+            <p>Publication Year: ${book.publicationYear}</p>
+            <p>Book: ${book.cover.i}</p>
+        `;
+        bookList.appendChild(bookElement);
+    });
+}
+
+document.querySelector('button').addEventListener('click',async() =>{
+    const query =document.querySelector('#query').ariaValueMax.trim();
+    const books = await searchBooks(query);
+    renderBooks(books);
+
+    if(!query){
+        alert('Please enter a term');
+        return;
+    }
+    try{
+        const book = fetchBooks(query);
+        renderBooks([book]);
+    }catch(error){
+        alert('Error fetching books');
+        console.error('error catching books',error);
+        return;
+    }
+})
+
