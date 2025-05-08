3dfc4fb1efef7c6b32bad26d07fd8a1173ddd100
alyssa-hdz <alyssa.hernandez619@gmail.com>
2025-05-08 18:59:53 -0500

    added logo image

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-// // import React from 'react';
-// import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-// import './App.css';
-// import Login from "./Login";
-// import Signup from "./Signup";
-// import Categories from "./Categories";
-// import Flashcards from "./Flashcards"
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-// function App() {
-//   return (
-//  <Router>
-//   <div className= "App">
-//     <Routes>
-//       <Route path = "/" element ={<h1>Study App</h1>}></Route>
-//       <Route path = "/login" element={<Login/>}/>
-//       <Route path = "/signup" element={<Signup/>}/>
-//       <Route path = "/categories" element={<Categories/>}/>
-//       <Route path = "/category/:id" element={<Flashcards/>}/>
-
-//     </Routes>
-//   </div>
-//  </Router>
-//   );
-// }
-
-// export default App;
-
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import Login from './Login';
 import Signup from './Signup';
 import Categories from './Categories';
@@ -40,26 +11,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* Navigation Bar */}
-        <nav className="navbar navbar-expand navbar-light bg-light px-3">
-          <Link className="navbar-brand" to="/">Study App</Link>
-          <div className="navbar-nav">
-            <Link className="nav-link" to="/login">Login</Link>
-            <Link className="nav-link" to="/signup">Signup</Link>
-            <Link className="nav-link" to="/categories">Categories</Link>
-          </div>
-        </nav>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img
+              src="/path/to/your/logo.png"
+              alt="App Logo"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {' Study-Fied'}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+              <Nav.Link as={Link} to="/categories">Categories</Nav.Link>
+              <Nav.Link as={Link} to="/flashcards">Flashcards</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<h1 className="text-center mt-4">Welcome to Study App</h1>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/category/:id" element={<Flashcards />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<h1 className="text-center mt-4">Welcome to Study App</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:id" element={<Flashcards />} />
+      </Routes>
     </Router>
   );
 }
